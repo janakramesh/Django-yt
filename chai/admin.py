a702140dcb7bc3fa2f11e810d8a6d15292c7da1e
janakramesh <janakramesh2003@gmail.com>
2026-03-22 21:48:09 +0530

    added relationships (model, admin, view)

--- chai/admin.py
+++ chai/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import chaiVerity
+from .models import chaiVerity, chaiReview, store, chaiCertification
 
 # Register your models here.
-admin.site.register(chaiVerity)
+class chaiReviewInLine(admin.TabularInline):
+    model = chaiReview
+    extra = 2
+
+class chaiVerityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [chaiReviewInLine]
+
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_verities')
+
+class chaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+
+admin.site.register(chaiVerity, chaiVerityAdmin)
+admin.site.register(store, storeAdmin)
+admin.site.register(chaiCertification, chaiCertificateAdmin)
+
+
+
